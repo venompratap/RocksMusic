c74b21b373e22c2d0cdd00e04a10e76ae1f1d7c3
Venom Pratap <venommusic91@gmail.com>
2025-06-17 14:16:19 +0530

    venom

--- VenomMusic/core/userbot.py
+++ VenomMusic/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="AviaxAss1",
+            name="VenomAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="AviaxAss2",
+            name="VenomAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="AviaxAss3",
+            name="VenomAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="AviaxAss4",
+            name="VenomAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="AviaxAss5",
+            name="VenomAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
